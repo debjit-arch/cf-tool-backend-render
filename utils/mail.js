d2a1d76a247d5f26b0d8175b258446bbf909ef55
debjit-arch <debjit@consultantsfactory.com>
2025-11-20 16:14:56 +0530

    Added Mail utility

---
+++ utils/mail.js
@@ -0,0 +1,23 @@
+// utils/mail.js
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS is not used on port 587
+  auth: {
+    user: "safesphere.cf@gmail.com", // your Gmail
+    pass: "agzgzywmujfuksbg",       // 16-char Gmail App Password, no spaces
+  },
+});
+
+const sendOtpEmail = async (to, otp) => {
+  await transporter.sendMail({
+    from: `"SafeSphere" <safesphere.cf@gmail.com>`,
+    to,
+    subject: "Your OTP for Password Reset",
+    text: `Your OTP for password reset is: ${otp}. It is valid for 10 minutes.`,
+  });
+};
+
+module.exports = { sendOtpEmail };
